9db632577adb5efac8f91b6e70ed1c96da771e1d
Dennis Granath <dennis@diabol.se>
2016-11-20 09:29:20 +0100

    Nil check response before closing it.

--- httpClient.go
+++ httpClient.go
@@ -1,114 +1,113 @@
 package main
 
 import (
-  "github.com/dghubble/sling"
-  log "github.com/Sirupsen/logrus"
-  "net/http"
-  "net/http/httputil"
-  //"reflect"
-  "encoding/json"
+	log "github.com/Sirupsen/logrus"
+	"github.com/dghubble/sling"
+	"net/http"
+	"net/http/httputil"
+	//"reflect"
+	"encoding/json"
 )
 
 var ServerBase *sling.Sling
 
-func createOrEditPipeline(payload map[string]interface {}){
-  debug, _ := json.MarshalIndent(payload, "", "    ")
-  log.Debug(string(debug))
-  pipelineName := payload["pipeline"].(map[string]interface{})["name"].(string)
-  log.Debug("Check if " + pipelineName + " exists.")
-  etag, _ := getEtag(pipelineName)
-  var response *http.Response
-  var err error
-  if etag != "" {
-    log.Debug(pipelineName + " allready exists. Making patch request")
-    response, err =  edit(payload, etag)
-  } else {
-    log.Debug("Creating new pipeline: " + pipelineName)
-    response, err = create(payload)
-  }
+func createOrEditPipeline(payload map[string]interface{}) {
+	debug, _ := json.MarshalIndent(payload, "", "    ")
+	log.Debug(string(debug))
+	pipelineName := payload["pipeline"].(map[string]interface{})["name"].(string)
+	log.Debug("Check if " + pipelineName + " exists.")
+	etag, _ := getEtag(pipelineName)
+	var response *http.Response
+	var err error
+	if etag != "" {
+		log.Debug(pipelineName + " allready exists. Making patch request")
+		response, err = edit(payload, etag)
+	} else {
+		log.Debug("Creating new pipeline: " + pipelineName)
+		response, err = create(payload)
+	}
 
-  if err != nil {
-    log.WithFields(log.Fields{
-      "error": err.Error(),
-    }).Warn("Could not create or edit pipeline: " + pipelineName)
-  }
-  if response.StatusCode != 200 {
-    log.WithFields(log.Fields{ "message": response.Body,}).Warn("Could not create or edit " + pipelineName + " reason: ")
-  }
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Warn("Could not create or edit pipeline: " + pipelineName)
+	}
+	if response.StatusCode != 200 {
+		log.WithFields(log.Fields{"message": response.Body}).Warn("Could not create or edit " + pipelineName + " reason: ")
+	}
 }
 
-func edit(payload map[string]interface {}, etag string) (*http.Response, error) {
-  path := "/go/api/admin/pipelines/" + payload["pipeline"].(map[string]interface{})["name"].(string)
-  return patchRequest(path, payload, etag)
+func edit(payload map[string]interface{}, etag string) (*http.Response, error) {
+	path := "/go/api/admin/pipelines/" + payload["pipeline"].(map[string]interface{})["name"].(string)
+	return patchRequest(path, payload, etag)
 }
 
-
-
-func create(payload map[string]interface {}) (*http.Response, error){
-  path := "/go/api/admin/pipelines"
-  return putRequest(path, payload)
+func create(payload map[string]interface{}) (*http.Response, error) {
+	path := "/go/api/admin/pipelines"
+	return putRequest(path, payload)
 
 }
-
 
 func getEtag(name string) (string, error) {
-  path := "/go/api/admin/pipelines/" + name
-  resp, err := getRequest(path)
-  if err != nil {
-    log.WithFields(log.Fields{
-      "error": err.Error(),
-    }).Warn("Could not get etag.")
-    return "", err
-  }
-  etag := resp.Header.Get("Etag")
+	path := "/go/api/admin/pipelines/" + name
+	resp, err := getRequest(path)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err.Error(),
+		}).Warn("Could not get etag.")
+		return "", err
+	}
+	etag := resp.Header.Get("Etag")
 
-  return etag, nil
+	return etag, nil
 }
 
 func getRequest(path string) (*http.Response, error) {
-    req, err := ServerBase.New().
-      Get(path).Request()
-    if err != nil {
-      return nil, err
-    }
-    return doRequest(req)
+	req, err := ServerBase.New().
+		Get(path).Request()
+	if err != nil {
+		return nil, err
+	}
+	return doRequest(req)
 }
 
-func putRequest(path string, payload map[string]interface {}) (*http.Response, error) {
+func putRequest(path string, payload map[string]interface{}) (*http.Response, error) {
 
-  req, err := ServerBase.New().
-    Post(path).BodyJSON(payload).Request()
-  if err != nil {
-    return nil, err
-  }
-  return doRequest(req)
+	req, err := ServerBase.New().
+		Post(path).BodyJSON(payload).Request()
+	if err != nil {
+		return nil, err
+	}
+	return doRequest(req)
 }
 
-func patchRequest(path string, payload map[string]interface{} ,etag string) (*http.Response, error) {
-  req, err := ServerBase.New().
-    Set("Content-Type", "application/json").
-    Set("If-Match", etag).
-    Put(path).BodyJSON(payload).Request()
-  if err != nil {
-    return nil, err
-  }
-  return doRequest(req)
+func patchRequest(path string, payload map[string]interface{}, etag string) (*http.Response, error) {
+	req, err := ServerBase.New().
+		Set("Content-Type", "application/json").
+		Set("If-Match", etag).
+		Put(path).BodyJSON(payload).Request()
+	if err != nil {
+		return nil, err
+	}
+	return doRequest(req)
 }
 
-func doRequest(request *http.Request) (*http.Response, error){
-  dump, _ := httputil.DumpRequest(request, true)
-  log.Debug(string(dump))
-  client := &http.Client{}
-  resp, err := client.Do(request)
-  dump,_ =  httputil.DumpResponse(resp, true)
-  log.Debug(string(dump))
-  defer resp.Body.Close()
-  return resp, err
+func doRequest(request *http.Request) (*http.Response, error) {
+	dump, _ := httputil.DumpRequest(request, true)
+	log.Debug(string(dump))
+	client := &http.Client{}
+	resp, err := client.Do(request)
+	dump, _ = httputil.DumpResponse(resp, true)
+	log.Debug(string(dump))
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	return resp, err
 }
 
-func init(){
-  ServerBase = sling.New().
-  SetBasicAuth(config.Username, config.Password).
-  Base(config.BaseUrl).
-  Set("Accept", "application/vnd.go.cd.v3+json")
+func init() {
+	ServerBase = sling.New().
+		SetBasicAuth(config.Username, config.Password).
+		Base(config.BaseUrl).
+		Set("Accept", "application/vnd.go.cd.v3+json")
 }
